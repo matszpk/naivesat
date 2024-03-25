dd03f32fa85f8c7be5fd1356495810e6924c9f24
matszpk <matszpk@interia.pl>
2024-03-25 15:01:12 +0100

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use gatenative::cpu_build_exec::*;
+use gatenative::mapper::*;
+use gatenative::opencl_build_exec::*;
+use gatenative::parseq_mapper::*;
+use gatenative::*;
+
+use opencl3::device::{get_all_devices, Device, CL_DEVICE_TYPE_GPU};
+
+fn main() {
+    println!("Hello, world!");
+}
