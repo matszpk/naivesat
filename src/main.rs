e8f11f216738ccb18805d28d18ac449ac2f9c8ee
matszpk <matszpk@interia.pl>
2024-03-25 15:25:44 +0100

    Add new stuff - exec type.

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,57 @@ use gatenative::opencl_build_exec::*;
 use gatenative::parseq_mapper::*;
 use gatenative::*;
 
+use clap::Parser;
 use opencl3::device::{get_all_devices, Device, CL_DEVICE_TYPE_GPU};
 
+use std::str::FromStr;
+
+#[derive(Clone, Copy, Debug)]
+enum ExecType {
+    ParCPU,
+    OpenCL(usize),
+    CPUAndOpenCL,
+    CPUAndOpenCLD,
+    CPUAndOpenCL1(usize),
+    CPUAndOpenCL1D(usize),
+}
+
+impl FromStr for ExecType {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, String> {
+        if s == "par_cpu" {
+            Ok(ExecType::ParCPU)
+        } else if s.starts_with("opencl:") {
+            Ok(ExecType::OpenCL(
+                usize::from_str(&s[7..]).map_err(|e| e.to_string())?,
+            ))
+        } else if s == "cpu_and_opencl" {
+            Ok(ExecType::CPUAndOpenCL)
+        } else if s == "cpu_and_opencl_d" {
+            Ok(ExecType::CPUAndOpenCLD)
+        } else if s.starts_with("cpu_and_opencl_1:") {
+            Ok(ExecType::CPUAndOpenCL1(
+                usize::from_str(&s[17..]).map_err(|e| e.to_string())?,
+            ))
+        } else if s.starts_with("cpu_and_opencl_1d:") {
+            Ok(ExecType::CPUAndOpenCL1D(
+                usize::from_str(&s[18..]).map_err(|e| e.to_string())?,
+            ))
+        } else {
+            Err("Unknown exec type".to_string())
+        }
+    }
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    circuit: String,
+    #[arg(short = 'E', long)]
+    exec_type: ExecType,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Hello, world!: {:?}", args.exec_type);
 }
