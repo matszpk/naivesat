606801b97834805cfd5812b93cd9b259397fccae
matszpk <matszpk@interia.pl>
2024-03-27 08:50:23 +0100

    Add naivesatostop.

---
+++ src/satostop.rs
@@ -0,0 +1,14 @@
+use gatenative::cpu_build_exec::*;
+use gatenative::mapper::*;
+use gatenative::opencl_build_exec::*;
+use gatenative::parseq_mapper::*;
+use gatenative::*;
+use gatesim::*;
+
+use clap::Parser;
+use opencl3::device::{get_all_devices, Device, CL_DEVICE_TYPE_GPU};
+
+use std::fs;
+
+fn main() {
+}
