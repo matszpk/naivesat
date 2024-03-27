d31190d3dd7cb07904fd7990a362bbdc49abe94c
matszpk <matszpk@interia.pl>
2024-03-27 12:21:39 +0100

    Update.

--- src/satostop.rs
+++ src/satostop.rs
@@ -10,5 +10,22 @@ use opencl3::device::{get_all_devices, Device, CL_DEVICE_TYPE_GPU};
 
 use std::fs;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct CommandArgs {
+    circuit: String,
+    unknowns: usize,
+    #[arg(short = 'e', long, default_value_t = 24)]
+    elem_inputs: usize,
+    #[arg(short = 'G', long)]
+    opencl_group_len: Option<usize>,
+}
+
 fn main() {
+    for x in get_all_devices(CL_DEVICE_TYPE_GPU).unwrap() {
+        println!("OpenCLDevice: {:?}", x);
+    }
+    let cmd_args = CommandArgs::parse();
+    let circuit_str = fs::read_to_string(cmd_args.circuit.clone()).unwrap();
+    let circuit = Circuit::<usize>::from_str(&circuit_str).unwrap();
 }
