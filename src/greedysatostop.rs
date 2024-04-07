d6b483fe0c902cfdc49b64f47bb83466ca5e7a83
matszpk <matszpk@interia.pl>
2024-04-07 16:34:40 +0200

    Write untested version of greedysatostop.

--- src/greedysatostop.rs
+++ src/greedysatostop.rs
@@ -1,24 +1,23 @@
 use gatenative::cpu_build_exec::*;
-use gatenative::opencl_build_exec::*;
+//use gatenative::opencl_build_exec::*;
 use gatenative::*;
 use gatesim::*;
 
 use clap::Parser;
-use opencl3::command_queue::CommandQueue;
-use opencl3::context::Context;
-use opencl3::device::{get_all_devices, Device, CL_DEVICE_TYPE_GPU};
-use opencl3::kernel::{ExecuteKernel, Kernel};
-use opencl3::memory::{Buffer, CL_MEM_READ_WRITE};
-use opencl3::program::Program;
-use opencl3::types::{cl_ulong, CL_BLOCKING};
+// use opencl3::command_queue::CommandQueue;
+// use opencl3::context::Context;
+// use opencl3::device::{get_all_devices, Device, CL_DEVICE_TYPE_GPU};
+// use opencl3::kernel::{ExecuteKernel, Kernel};
+// use opencl3::memory::{Buffer, CL_MEM_READ_WRITE};
+// use opencl3::program::Program;
+// use opencl3::types::{cl_ulong, CL_BLOCKING};
 
 use rayon::prelude::*;
 
-use std::cell::UnsafeCell;
 use std::fs;
 use std::ops::Range;
 use std::str::FromStr;
-use std::sync::atomic::{self, AtomicU32, AtomicU64};
+use std::sync::atomic::{self, AtomicU32};
 use std::sync::{Arc, Mutex};
 use std::time::SystemTime;
 
@@ -31,17 +30,6 @@ use std::time::SystemTime;
 struct CommandArgs {
     circuit: String,
     unknowns: usize,
-    #[arg(short = 'C', long)]
-    opencl: Option<usize>,
-    #[arg(short = 'P', long, default_value_t = 1)]
-    partitions: usize,
-    #[arg(
-        short = 'Q',
-        long,
-        default_value_t = 2,
-        help = "How many partitions contains main partitions"
-    )]
-    main_partition_mult: usize,
     #[arg(short = 'v', long)]
     verify: bool,
 }
@@ -58,9 +46,106 @@ enum FinalResult {
     NoSolution,
 }
 
+struct AtomicU32Array<'a> {
+    original: Vec<u32>,
+    atomic: &'a [AtomicU32],
+}
+
+impl From<Vec<u32>> for AtomicU32Array<'_> {
+    fn from(mut t: Vec<u32>) -> Self {
+        let atomic = unsafe {
+            &*std::ptr::slice_from_raw_parts(
+                t.as_mut_slice().as_mut_ptr().cast::<AtomicU32>(),
+                t.len(),
+            )
+        };
+        Self {
+            original: t,
+            atomic,
+        }
+    }
+}
+
+impl<'a> AtomicU32Array<'a> {
+    #[inline]
+    fn get(&self, i: usize) -> &'a AtomicU32 {
+        &self.atomic[i]
+    }
+
+    #[inline]
+    fn as_slice(&self) -> &'a [AtomicU32] {
+        &self.atomic
+    }
+
+    #[inline]
+    fn len(&self) -> usize {
+        self.original.len()
+    }
+}
+
 //
 // join nexts
 //
+
+fn join_nexts(input_len: usize, nexts: Arc<AtomicU32Array>) {
+    let cpu_num = rayon::current_num_threads();
+    let chunk_num = std::cmp::min(std::cmp::max(cpu_num * 8, 64), nexts.len() >> 6);
+    let chunk_len = nexts.len() / chunk_num;
+    let stop_mask = 1u32 << input_len;
+    let next_mask = stop_mask - 1u32;
+    nexts
+        .as_slice()
+        .chunks(chunk_len)
+        .par_bridge()
+        .for_each(|chunk| {
+            for cell in chunk {
+                std::sync::atomic::fence(atomic::Ordering::SeqCst);
+                let old_value = cell.load(atomic::Ordering::SeqCst);
+                let old_next = old_value & next_mask;
+                std::sync::atomic::fence(atomic::Ordering::SeqCst);
+                if (old_value & stop_mask) == 0 {
+                    cell.store(
+                        nexts.get(old_next as usize).load(atomic::Ordering::SeqCst),
+                        atomic::Ordering::SeqCst,
+                    );
+                }
+                std::sync::atomic::fence(atomic::Ordering::SeqCst);
+            }
+        });
+}
+
+fn find_solution(
+    input_len: usize,
+    unknowns: usize,
+    nexts: Arc<AtomicU32Array>,
+) -> Option<Solution> {
+    let unknown_state_num = 1 << unknowns;
+    let cpu_num = rayon::current_num_threads();
+    let chunk_num = std::cmp::min(std::cmp::max(cpu_num * 8, 64), unknown_state_num >> 6);
+    let chunk_len = unknown_state_num / chunk_num;
+    let stop_mask = 1u32 << input_len;
+    let next_mask = stop_mask - 1;
+    let unknowns_mult = 1 << (input_len - unknowns);
+    let result = Mutex::new(None);
+    nexts
+        .as_slice()
+        .chunks(unknowns_mult * chunk_len)
+        .enumerate()
+        .par_bridge()
+        .for_each(|(ch_idx, chunk)| {
+            for (i, v) in chunk.chunks(unknowns_mult).enumerate() {
+                let value = v[0].load(atomic::Ordering::SeqCst);
+                if (value & stop_mask) != 0 {
+                    let mut r = result.lock().unwrap();
+                    *r = Some(Solution {
+                        start: (i + (ch_idx * chunk_len)) as u64,
+                        end: (value & next_mask) as u64,
+                    });
+                }
+            }
+        });
+    result.into_inner().unwrap()
+}
 
 //
 // main solver code
@@ -90,43 +175,48 @@ const AGGR_OUTPUT_CPU_CODE: &str = r##"{
 #endif
 }"##;
 
-fn do_solve_with_cpu_builder(
-    circuit: Circuit<usize>,
-    unknowns: usize,
-    elem_inputs: usize,
-    unknown_fill_bits: usize,
-    cmd_args: &CommandArgs,
-) -> FinalResult {
-    let mut builder = CPUBuilder::new_parallel(None, Some(4096));
+fn do_solve_with_cpu_builder(circuit: Circuit<usize>, cmd_args: &CommandArgs) -> FinalResult {
     let input_len = circuit.input_len();
     let output_len = input_len + 1;
-    let arg_steps = 1u128 << (input_len - elem_inputs);
-    builder.transform_helpers();
-    builder.user_defs(&format!("#define OUTPUT_NUM ({})\n", output_len));
-    builder.user_defs(&gen_output_transform_code(output_len));
     let words_per_elem = (output_len + 31) >> 5;
-    builder.add_with_config(
-        "formula",
-        circuit,
-        CodeConfig::new()
-            .elem_inputs(Some(&(0..input_len).collect::<Vec<usize>>()))
-            .aggr_output_code(Some(AGGR_OUTPUT_CPU_CODE))
-            .aggr_output_len(Some(words_per_elem * (1 << input_len)))
-            .dont_clear_outputs(true),
-    );
-    let mut execs = builder.build().unwrap();
-    let input = execs[0].new_data(16);
-    let output = execs[0].execute(&input, 0).unwrap().release();
-    FinalResult::NoSolution
+    let (output, start) = {
+        let mut builder = CPUBuilder::new_parallel(None, Some(2048));
+        builder.transform_helpers();
+        builder.user_defs(&format!("#define OUTPUT_NUM ({})\n", output_len));
+        builder.user_defs(&gen_output_transform_code(output_len));
+        builder.add_with_config(
+            "formula",
+            circuit,
+            CodeConfig::new()
+                .elem_inputs(Some(&(0..input_len).collect::<Vec<usize>>()))
+                .aggr_output_code(Some(AGGR_OUTPUT_CPU_CODE))
+                .aggr_output_len(Some(words_per_elem * (1 << input_len)))
+                .dont_clear_outputs(true),
+        );
+        let mut execs = builder.build().unwrap();
+        let start = SystemTime::now();
+        let input = execs[0].new_data(16);
+        (execs[0].execute(&input, 0).unwrap().release(), start)
+    };
+    let nexts = Arc::new(AtomicU32Array::from(output));
+    let mut final_result = FinalResult::NoSolution;
+    for i in 0..input_len {
+        println!("Stage: {} / {}", i, input_len);
+        join_nexts(input_len, nexts.clone());
+        if let Some(sol) = find_solution(input_len, cmd_args.unknowns, nexts.clone()) {
+            final_result = FinalResult::Solution(sol);
+            break;
+        }
+    }
+    let time = start.elapsed().unwrap();
+    println!("Time: {}", time.as_secs_f64());
+    final_result
 }
 
 fn do_solve(circuit: Circuit<usize>, cmd_args: CommandArgs) {
-    let partitions = std::cmp::max(1, cmd_args.partitions);
-    let main_partition_mult = std::cmp::max(1, cmd_args.main_partition_mult);
     let input_len = circuit.input_len();
     assert!(input_len < 32);
-    let result = Some(FinalResult::NoSolution);
-    let result = result.unwrap();
+    let result = do_solve_with_cpu_builder(circuit.clone(), &cmd_args);
     if let FinalResult::Solution(sol) = result {
         println!("Solution: {:?}", sol);
         if cmd_args.verify {
@@ -156,9 +246,9 @@ fn do_solve(circuit: Circuit<usize>, cmd_args: CommandArgs) {
 }
 
 fn main() {
-    for x in get_all_devices(CL_DEVICE_TYPE_GPU).unwrap() {
-        println!("OpenCLDevice: {:?}", x);
-    }
+    // for x in get_all_devices(CL_DEVICE_TYPE_GPU).unwrap() {
+    //     println!("OpenCLDevice: {:?}", x);
+    // }
     let cmd_args = CommandArgs::parse();
     let circuit_str = fs::read_to_string(cmd_args.circuit.clone()).unwrap();
     let circuit = Circuit::<usize>::from_str(&circuit_str).unwrap();
